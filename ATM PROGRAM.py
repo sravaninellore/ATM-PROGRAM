381bca37d5006d28ca2a379921d623d17f384618
sravaninellore <85183913+sravaninellore@users.noreply.github.com>
2021-07-03 14:56:36 +0530

    Add files via upload

---
+++ ATM PROGRAM.py
@@ -0,0 +1,53 @@
+import randam
+class account:
+print("Welcome to canara bank!")
+#asking user to insert the card
+print("Please insert your card")
+#asking pin from user
+while True:
+    pin = int(input("Enter your pin:"))
+    if pin is correct:
+        print("Then it will proceed to transistion")
+    else:
+        print("Wrong pin!,Try again")
+#account session
+while True:
+    print("\n1-Balance enquirey \n2-Withdrawl \n3-Deposit \n4-Pin change")
+    opinion = int(input("\n enter your opinion:"))
+#balance enquirey
+ if oipnion = "Balance enquirey":
+     print(account.getbalance())
+#withdrawl
+while True:
+    elif opinion = "Withdrawl":
+    amount = int(input("Enter the amount:"))
+    if amount > 0:
+        print("Collect your cash")
+    else:
+        print("Please enter the valid amount")
+#deposit
+while True:
+    elif opinion = "Deopsit":
+        deposit = int(input("Enter your amount to deposit:"))
+        if deposit > 0:
+            print("Your deposit is sucessfully done")
+        else:
+            print("Enter valid amount to proceed")
+#pinchange
+while True:
+    elif opinion = "Pin change":
+    Pin_change = input("Enter here new pin:")
+    print("Your pin has been sucessfully changed")
+#anyother choice
+while True:
+    elif opinion = "anyother choice"
+    print("That is an invalid choice")
+
+      
+            
+            
+     
+    
+            
+    
+        
